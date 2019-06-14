d26a439f7a7eaf136eaa9d062bc95fbfdf6544ab
Yusuke Endoh <mame@ruby-lang.org>
2019-06-14 12:36:42 +0900

    Misc. for rubocop

--- lib/optcarrot/config.rb
+++ lib/optcarrot/config.rb
@@ -105,8 +105,8 @@ module Optcarrot
         @options = DEFAULT_OPTIONS.dup
         parse_option until @argv.empty?
         error "ROM file is not given" unless @options[:romfile]
-      rescue Invalid => err
-        puts "[FATAL] #{ err }"
+      rescue Invalid => e
+        puts "[FATAL] #{ e }"
         exit 1
       end
 

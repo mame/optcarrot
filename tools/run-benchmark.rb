537b265981f078aa2076bd8953e9a05e516a40aa
sisshiki1969 <sisshiki@mac.com>
2021-03-23 17:57:51 +0900

    use %w(...) instead of %(...).

--- tools/run-benchmark.rb
+++ tools/run-benchmark.rb
@@ -160,7 +160,7 @@ end
 ###############################################################################
 
 # https://github.com/rbenv/ruby-build/wiki
-MASTER_APT = %(
+MASTER_APT = %w(
   autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6
   libgdbm-dev libdb-dev git ruby
 )
